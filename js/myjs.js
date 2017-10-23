0534be4d0fe0b6a6c0bccb956df3819832e089aa
cdavidmr <cdavidmr@outlook.com>
2017-10-23 02:04:11 -0500

    DOM Pratice 1, get/set/remove class(css) and change textContent

--- js/myjs.js
+++ js/myjs.js
@@ -1,12 +1,19 @@
-var v = true;
-
 function TestDom() {
 	var p1 = document.getElementById("par1");
-	if (v == true) {
-		p1.innerHTML = "True";
-		v = false;
-	} else {
-		p1.innerHTML = "False";
-		v = true;
-	}
+	var p2 = document.getElementById("par2");
+	//get class
+	let pc1 = p1.getAttribute("class");
+	let pc2 = p2.getAttribute("class");
+	//aux var
+	let pp1 = p1.textContent;
+	//change text
+	p1.innerHTML = p2.textContent;
+	p2.innerHTML = pp1;
+	//remove class 
+	p1.removeAttribute("class");
+	p2.removeAttribute("class");
+	//Set class 
+	p1.setAttribute("class",pc2);
+	p2.setAttribute("class",pc1);
+	console.log(pc1+pc2+pp1);
 }
