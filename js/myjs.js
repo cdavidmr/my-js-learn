16616b7198ab7375baa8f868f33e27b85476085f
cdavidmr <cdavidmr@outlook.com>
2017-10-23 22:41:16 -0500

    ..Update..

--- js/myjs.js
+++ js/myjs.js
@@ -20,8 +20,27 @@ function TestDom() {
 	var color = ["darkred","darkblue","darkgreen","black","purple","gold","tomato","#ee82ee"];
 	var rdm = Math.floor(Math.random()*color.length);
 	//
+	bo1.classList.remove("bg-dark")
 	bo1.style.backgroundColor = color[rdm];
 	//
 	console.log(pc1+pc2+pp1);
 	console.log(color[rdm]);
+}
+
+function lamp() {
+	let btn_on = document.getElementById("btn_on");
+	let btn_off = document.getElementById("btn_off");
+	let img_lamp = document.getElementById("lamp_img");
+
+	if(btn_on.hasAttribute("disabled","") == true){
+		btn_on.removeAttribute("disabled","");
+		btn_off.setAttribute("disabled","");
+		img_lamp.setAttribute("src","img/lamp-off.png");
+	}else{
+		btn_on.setAttribute("disabled","");
+		btn_off.removeAttribute("disabled","");
+		img_lamp.setAttribute("src","img/lamp-on.png");
+	}
+
+
 }
